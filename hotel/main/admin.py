898878a2f6fb648e29e78544b1d0985fd3c51180
slobodianiukdenys <144223224+slobodianiukdenys@users.noreply.github.com>
2025-04-15 01:01:23 +0300

    Add files via upload

--- hotel/main/admin.py
+++ hotel/main/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Client
+from .models import Client, Room, Booking, Service
 # Register your models here.
 
 admin.site.register(Client)
+admin.site.register(Room)
+admin.site.register(Booking)
+admin.site.register(Service)
